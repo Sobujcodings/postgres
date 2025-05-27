2a3816196a3080a06fa82c3202afeb648444ca17
merajsobuj <merajsobuj@gmail.com>
2025-05-28 04:28:26 +0600

    code

--- triggers.sql
+++
@@ -1,4 +0,0 @@
--- triggers (akta obj jeta automatically run hobe jokhon amr database e certain kono event ghotbe tokhon fire hobe)
--- usage -> suppose akta user k delete kora holo -> akhn ami aita tract rakhte chacchi j kon user k/kano/kokhon 
--- delete kora holo sheta arekta table store thakbe => akta user delete korar por ami ata automatically shei function
---  trigger/run kore dibo je ai detials jate onno table e store hoy
